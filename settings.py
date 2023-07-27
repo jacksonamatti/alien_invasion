9ac18ad6808df616359b8da4155acad66403eae3
jackson amatti <amatti.dev@gmail.com>
2023-07-27 17:41:18 -0300

    criacao da classe de configuracoes do jogo

---
+++ settings.py
@@ -0,0 +1,11 @@
+'''CLASSE DE CONFIGURAÇÃO GERAL DO PROJETO'''
+
+class Settings():
+    def __init__(self):
+        #inicia as configurações do jogo
+        
+        #configuracao de tela
+        self.scree_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+        
