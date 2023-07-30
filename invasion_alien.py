aa73c4e2bbdf7cfca4f2f75449e921552468f27b
jackson amatti <amatti.dev@gmail.com>
2023-07-30 14:39:32 -0300

    refatoracao da funcao ckeckevent mais melhorias no readme do projeto

--- invasion_alien.py
+++ invasion_alien.py
@@ -6,7 +6,7 @@ import game_function as gf # usando o 'as' que siginifica um apelido para a func
 def run_game():
     #inicaliza o jogo e cria o objeto tela 
     pygame.init()
-    ai_settings = Settings() # instancia o arquivo settings para poder usar 
+    ai_settings = Settings() # aqui esta armazenado as configuracoes 
     screen = pygame.display.set_mode((
         ai_settings.scree_width, ai_settings.screen_height
         )) #cria a janela de exibição
