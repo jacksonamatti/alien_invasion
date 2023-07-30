c7cf9d7005b339f2522cee348cd88945d7f3e2d2
jackson amatti <amatti.dev@gmail.com>
2023-07-30 15:07:03 -0300

    adicionando projeteis ao projeto

--- settings.py
+++ settings.py
@@ -10,4 +10,11 @@ class Settings():
         self.bg_color = (230,230,230)
         self.ship_speed_factor = 1.5
         
+        #projeteis
+        
+        self.bullets_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color =  60,60,60
+        
         
